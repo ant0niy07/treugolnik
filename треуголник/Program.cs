f2794a369f0c45291a3625052f0ce088a8a92578
ant0niy07 <86295276+ant0niy07@users.noreply.github.com>
2023-10-22 15:28:35 +0200

    Add files via upload

---
+++ треуголник/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace треуголник
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Введите длины трех сторон треугольника:");
+
+            Console.Write("Сторона A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Сторона B: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Сторона C: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            if (IsIsoscelesTriangle(a, b, c))
+            {
+                Console.WriteLine("Этот треугольник равнобедренный.");
+            }
+            else
+            {
+                Console.WriteLine("Этот треугольник не является равнобедренным.");
+            }
+
+            //Это я сделал для того чтобы консоль не закрывалась и было возможно прочитать ответ консоли
+            Console.WriteLine("Нажмите пожалуйста любую клавишу для закрытия консоли!");
+
+
+            Console.ReadLine();
+
+        }
+
+        static bool IsIsoscelesTriangle(double a, double b, double c)
+        {
+            return a == b || b == c || a == c;
+        }
+    }
+}
